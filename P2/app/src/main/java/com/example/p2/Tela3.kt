5d5ef61ac0686452c90c39e3c17df5a9137e8ebb
CaiooSouzaa <caioggsouza@hotmail.com>
2025-05-04 23:02:55 -0300

    Novo commit

--- P2/app/src/main/java/com/example/p2/Tela3.kt
+++ P2/app/src/main/java/com/example/p2/Tela3.kt
@@ -1,36 +1,20 @@
 package com.example.p2
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Tela2 : AppCompatActivity() {
+class Tela3 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tela2)
+        setContentView(R.layout.activity_tela3)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-    }
-    fun Tela3(view: View){
-        var tela3 = Intent(this, Tela3::class.java)
-        startActivity(tela3)
-    }
-
-    fun Tela4(view: View){
-        var tela4 = Intent(this, Tela4::class.java)
-        startActivity(tela4)
-    }
-
-    fun Tela5(view: View){
-        var tela5 = Intent(this, Tela5::class.java)
-        startActivity(tela5)
     }
 }
