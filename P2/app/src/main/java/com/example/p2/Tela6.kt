5a9aab0fce70f57810fb09eef1d24a3904e9c768
CaiooSouzaa <caioggsouza@hotmail.com>
2025-05-06 00:29:23 -0300

    Alteracao nas telas 3 e 4, criacao da tela 6 para editar alunos

--- P2/app/src/main/java/com/example/p2/Tela6.kt
+++ P2/app/src/main/java/com/example/p2/Tela6.kt
@@ -1,32 +1,20 @@
 package com.example.p2
 
 import android.os.Bundle
-import android.view.inputmethod.EditorInfo
-import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Tela3 : AppCompatActivity() {
+class Tela6 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_tela3)
+        setContentView(R.layout.activity_tela6)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-        val nomeAluno = findViewById<EditText>(R.id.nomeAluno)
-        val cpfAluno = findViewById<EditText>(R.id.cpfAluno)
-        nomeAluno.setOnEditorActionListener { _, actionId, _ ->
-            if (actionId == EditorInfo.IME_ACTION_NEXT) {
-                cpfAluno.post { cpfAluno.requestFocus() }
-                true
-            } else {
-                false
-            }
         }
     }
 }
