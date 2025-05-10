02db57c7dfbc4a657aef6131f0d7728c60e0ca96
CaiooSouzaa <caioggsouza@hotmail.com>
2025-05-10 02:24:45 -0300

    Instalaçao do firebase

--- P2/app/src/main/java/com/example/p2/MainActivity.kt
+++ P2/app/src/main/java/com/example/p2/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.Firebase
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +19,8 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        Firebase
     }
 
     fun Tela2(view: View) {
