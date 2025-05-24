59eeed7ccbb93bbfe0042c9c0d10d33712fad28a
CaiooSouzaa <caioggsouza@hotmail.com>
2025-05-24 08:29:50 -0300

    New commit

--- P2IGOR/app/src/main/java/com/example/p2_igor/Tela3.kt
+++ P2IGOR/app/src/main/java/com/example/p2_igor/Tela3.kt
@@ -1,6 +1,8 @@
 package com.example.p2_igor
 
 import android.os.Bundle
+import android.view.inputmethod.EditorInfo
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,34 @@ class Tela3 : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val nomeAluno = findViewById<EditText>(R.id.nomeAluno)
+        val cpfAluno = findViewById<EditText>(R.id.cpfAluno)
+        val instituicaoAluno = findViewById<EditText>(R.id.instituicaoAluno)
+        val alturaAluno = findViewById<EditText>(R.id.alturaAluno)
+        val pesoAluno = findViewById<EditText>(R.id.pesoAluno)
+        val telefoneALuno = findViewById<EditText>(R.id.telefoneAluno)
+        val idadeAluno = findViewById<EditText>(R.id.idadeAluno)
+
+        val actionId = EditorInfo.IME_ACTION_NEXT
+
+        nomeAluno.setOnEditorActionListener { _, actionId, _ ->
+            when (actionId) {
+                EditorInfo.IME_ACTION_NEXT -> {
+                    cpfAluno.post { cpfAluno.requestFocus() }
+                    true
+                }
+
+                EditorInfo.IME_ACTION_NEXT -> {
+                    instituicaoAluno.post { instituicaoAluno.requestFocus() }
+                    true
+                }
+
+                else -> {
+                    false
+                }
+            }
+
         }
     }
 }
