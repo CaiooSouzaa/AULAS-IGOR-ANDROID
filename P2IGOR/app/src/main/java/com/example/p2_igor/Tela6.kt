8bc8b0c7a8912be407686143a1c8f9753f9e54cb
CaiooSouzaa <caioggsouza@hotmail.com>
2025-05-24 08:38:24 -0300

    New commit

---
+++ P2IGOR/app/src/main/java/com/example/p2_igor/Tela6.kt
@@ -0,0 +1,29 @@
+package com.example.p2_igor
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Tela6 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tela6)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun tela2(view: View){
+        var intent = Intent(this, Tela2::class.java)
+        Toast.makeText(this, "Cadastro atualizado", Toast.LENGTH_LONG).show()
+        startActivity(intent)
+    }
+}
---
+++ P2IGOR/app/src/main/java/com/example/p2_igor/Tela7.kt
@@ -0,0 +1,26 @@
+package com.example.p2_igor
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Tela7 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_tela7)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+    fun MainActivity(view: View){
+        var intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+}
